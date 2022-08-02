762be3ec94c6db82d0cc3cc437451ba717d52776
Sam Ashurst <95435776+SamAshurst@users.noreply.github.com>
2022-08-02 12:33:02 +0100

    Adds card styling to programs

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useParams, useLocation } from "react-router-dom";
-import { Card, Container, Col } from "react-bootstrap";
+import { Card, Container, Col, Row } from "react-bootstrap";
 import * as api from "../utils/api";
 
 export default function Category() {
@@ -34,25 +34,27 @@ export default function Category() {
     <>
       <div>{params.channel}</div>
       <div>{params.id}</div>
-      <Container className="p-3 m-auto">
-        {programList.map((program) => {
-          return (
-            <Col className="d-flex" key={program.id}>
-              <Card border="dark">
-                <Card.Img
-                  variant="top"
-                  className="Program-image"
-                  src={program.programimage}
-                />
-                <Card.Body>
-                  <Card.Title>{program.name}</Card.Title>
-                  <Card.Text>{program.description}</Card.Text>
-                  <Card.Text>{program.programurl}</Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-          );
-        })}
+      <Container className="p-1 m-auto">
+        <Row xs={2} sm={2} md={3} lg={4} xl={6} className="g-4 d-flex m-auto">
+          {programList.map((program) => {
+            return (
+              <Col className="d-flex" key={program.id}>
+                <Card border="dark">
+                  <Card.Img
+                    variant="top"
+                    className="Program-image"
+                    src={program.programimage}
+                  />
+                  <Card.Body className="p-1">
+                    <Card.Title>{program.name}</Card.Title>
+                    <Card.Text>{program.description}</Card.Text>
+                    <Card.Text>{program.programurl}</Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+            );
+          })}
+        </Row>
       </Container>
     </>
   );
