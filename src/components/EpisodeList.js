2b0dae52c0981ef55c4d72e667f4ba8123c19ebb
Sam Ashurst <95435776+SamAshurst@users.noreply.github.com>
2022-08-25 10:24:10 +0100

    Makes episode list match overall style

--- src/components/EpisodeList.js
+++ src/components/EpisodeList.js
@@ -1,13 +1,6 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import {
-  Container,
-  Col,
-  Row,
-  Button,
-  ListGroup,
-  ListGroupItem,
-} from "react-bootstrap";
+import { Container, Col, Row, Button, ListGroup } from "react-bootstrap";
 import * as api from "../utils/api";
 import publishedDate from "../utils/TimeConverter";
 
@@ -23,7 +16,6 @@ export default function Episodes() {
     });
   }, [params.programId]);
 
-  console.log(episodeList);
   if (isLoading) {
     return <div>Loading..</div>;
   }
@@ -45,23 +37,23 @@ export default function Episodes() {
       <ListGroup>
         {episodeList.map((episode) => {
           return (
-            <a
-              className="episodes-list-link"
-              href={episode.url}
-              rel="noopener noreferrer"
-            >
-              <ListGroupItem className="p-0" key={episode.id}>
+            <ListGroup.Item className="p-0" key={episode.id}>
+              <a
+                className="episodes-list-link"
+                href={episode.url}
+                rel="noopener noreferrer"
+              >
                 <Container fluid className="episodes-container ps-2 pe-2 pb-2">
                   <Row className="d-grid">
-                    <Col className="episodes-image-col">
+                    <Col className="episodes-image-col m-auto">
                       <img
                         className="episodes-image"
                         src={episode.imageurl}
                         alt="episodes-thumbnail"
                       ></img>
                     </Col>
-                    <Col>
-                      <div>
+                    <Row>
+                      <div className="m-auto">
                         <div className="fw-bold episodes-title">
                           {episode.title}
                         </div>
@@ -72,11 +64,11 @@ export default function Episodes() {
                           {publishedDate(episode.publishdateutc)}
                         </div>
                       </div>
-                    </Col>
+                    </Row>
                   </Row>
                 </Container>
-              </ListGroupItem>
-            </a>
+              </a>
+            </ListGroup.Item>
           );
         })}
       </ListGroup>
