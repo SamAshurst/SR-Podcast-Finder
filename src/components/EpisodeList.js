9fc99318e3a56e38f29aa8dd26b07e61c337487b
Sam Ashurst <95435776+SamAshurst@users.noreply.github.com>
2022-08-25 09:46:42 +0100

    List item links to episodes url

--- src/components/EpisodeList.js
+++ src/components/EpisodeList.js
@@ -23,6 +23,7 @@ export default function Episodes() {
     });
   }, [params.programId]);
 
+  console.log(episodeList);
   if (isLoading) {
     return <div>Loading..</div>;
   }
@@ -44,32 +45,38 @@ export default function Episodes() {
       <ListGroup>
         {episodeList.map((episode) => {
           return (
-            <ListGroupItem className="p-0" key={episode.id}>
-              <Container fluid className="episodes-container ps-2 pe-2 pb-2">
-                <Row className="d-grid">
-                  <Col className="episodes-image-col">
-                    <img
-                      className="episodes-image"
-                      src={episode.imageurl}
-                      alt="episodes-thumbnail"
-                    ></img>
-                  </Col>
-                  <Col>
-                    <div>
-                      <div className="fw-bold episodes-title">
-                        {episode.title}
+            <a
+              className="episodes-list-link"
+              href={episode.url}
+              rel="noopener noreferrer"
+            >
+              <ListGroupItem className="p-0" key={episode.id}>
+                <Container fluid className="episodes-container ps-2 pe-2 pb-2">
+                  <Row className="d-grid">
+                    <Col className="episodes-image-col">
+                      <img
+                        className="episodes-image"
+                        src={episode.imageurl}
+                        alt="episodes-thumbnail"
+                      ></img>
+                    </Col>
+                    <Col>
+                      <div>
+                        <div className="fw-bold episodes-title">
+                          {episode.title}
+                        </div>
+                        <div className=" episodes-text">
+                          {episode.description}
+                        </div>
+                        <div className="text-muted episodes-published">
+                          {publishedDate(episode.publishdateutc)}
+                        </div>
                       </div>
-                      <div className=" episodes-text">
-                        {episode.description}
-                      </div>
-                      <div className="text-muted episodes-published">
-                        {publishedDate(episode.publishdateutc)}
-                      </div>
-                    </div>
-                  </Col>
-                </Row>
-              </Container>
-            </ListGroupItem>
+                    </Col>
+                  </Row>
+                </Container>
+              </ListGroupItem>
+            </a>
           );
         })}
       </ListGroup>
