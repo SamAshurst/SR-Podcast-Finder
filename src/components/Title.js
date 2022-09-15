8b6dd992fc5766d7e570ad78257b5fd588866c98
Sam Ashurst <95435776+SamAshurst@users.noreply.github.com>
2022-09-15 11:08:42 +0200

    Header changes colour on hover

--- src/components/Title.js
+++ src/components/Title.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Title() {
   return (
-    <header className="mt-3 mb-4">
+    <header className="mt-3 mb-4 header-test">
       <Link to="/" className="header-title">
         <h1>Sveriges Radio Podcast Sökare</h1>
       </Link>
